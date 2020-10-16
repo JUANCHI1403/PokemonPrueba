0819c6a7e136781407d1757ed5747eac1418bfee
Carlos <carlos.rojas.irarrazabal@gmail.com>
2020-10-16 17:00:06 -0300

    se agrega canvas

--- index.js
+++ index.js
@@ -1,22 +1,56 @@
-$(document).ready(function () {
-    consultarAPI("pikachu")
-});
-
-// Conectando boton con input
-$("button").click(function () {
-    let pokemon = $("#input").val()
-    consultarAPI(pokemon)
-}
-)
-
-function consultarAPI(nombrePokemon) {
-    $.ajax({
-        type: "GET",
-        url: `https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`,
-        success: function (data) {
-            $("#Pokemon").html(data.name)
+$(document).ready(function(){
+    consultaAPI("pikachu")
+    canvas()
+    
+    })
+    
+    $("button").click(function(){
+        let pokemon = $("#input").val()
+        consultaAPI(pokemon)
+    })
+    
+    function consultaAPI(nombrePokemon) {
+        $.ajax({
+            type: "GET",
+            url: `https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`,
+            success: function (data) {
+                
+                $("#pokemon").html(data.name)
+                let defensa = data.stats[2].base_stat
+                canvas(defensa)
+            },
+            dataType: 'json'
+        });
+    }
+    
+    
+    
+    
+    // Canvas
+    
+    function canvas(defensa){
+        var chart = new CanvasJS.Chart("chartContainer", {
+        theme: "light2", // "light1", "light2", "dark1", "dark2"
+        exportEnabled: true,
+        animationEnabled: true,
+        title: {
+            text: "Desktop Browser Market Share in 2016"
         },
-        dataType: 'json',
+        data: [{
+            type: "pie",
+            startAngle: 25,
+            toolTipContent: "<b>{label}</b>: {y}",
+            showInLegend: "true",
+            legendText: "{label}",
+            indexLabelFontSize: 16,
+            indexLabel: "{label} - {y}",
+            dataPoints: [
+                { y: defensa, label: "defensa" },
+            ]
+        }]
     });
-}
-
+    chart.render();
+    }
+    
+    
+    
