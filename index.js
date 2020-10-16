37d5811d5a308e3d09ddc8271e9f6320593912a4
PedroManuel <pedromanuelrivas@gmail.com>
2020-10-16 16:23:34 -0300

    funcionalidad de boton y de busqueda en API

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 $(document).ready(function () {
+    consultarAPI("pikachu")
+});
 
+// Conectando boton con input
+$("button").click(function () {
+    let pokemon = $("#input").val()
+    consultarAPI(pokemon)
+}
+)
+
+function consultarAPI(nombrePokemon) {
     $.ajax({
         type: "GET",
-        url: "https://pokeapi.co/api/v2/pokemon/ditto",
+        url: `https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`,
         success: function (data) {
             $("#Pokemon").html(data.name)
         },
         dataType: 'json',
     });
-});
+}
 
